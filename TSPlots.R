4b3407b6c9054c3aa55b4928fe51219f7b58a233
Brigitte Dorner <bdorner@driftwoodcove.ca>
2020-04-27 12:00:33 -0700

    Improvements to display logic when selection is empty

--- TSPlots.R
+++ TSPlots.R
@@ -1,24 +1,26 @@
 tsPlots.makeCUtable <- function(CUs) {
-  tagList(
-    tags$div(class='full-sparkline-box-header', 'CUs currently highlighted'),
-    spark.makeCUSparklineTable(CUs, mode='full')
-  )
+  if (length(CUs) > 0)
+    tagList(
+      tags$div(class='full-sparkline-box-header', 'CUs currently highlighted'),
+      spark.makeCUSparklineTable(CUs, mode='full')
+    )
+  else
+    tags$div('There are currently no CUs highlighted')
 }
 
 tsPlots.makePopTable <- function(pops) {
-  p <- tags$div(class='full-sparkline-box-header', 'Sites currently highlighted')
-  if (length(pops) > 0) {
-    p <- tagList(p, spark.makePopSparklineTable(pops, mode='full'))
-  }
-  p
+  if (length(pops) > 0) 
+    tagList(tags$div(class='full-sparkline-box-header', 'Sites currently highlighted'), 
+            spark.makePopSparklineTable(pops, mode='full'))
+  else
+    tags$div('There are currently no CUs highlighted')
 }
 
-
 output$box_TSPlots <- renderUI({
-    p <- tsPlots.makeCUtable(data.currentSelection[['CUs']])
-    if(data.showPops()) 
-      p <- tagList(p, tags$hr(), tsPlots.makePopTable(data.currentSelection[['Pops']]))
-    p})
+  p <- tsPlots.makeCUtable(data.currentSelection[['CUs']])
+  if (data.showPops() && !data.currentSelectionEmpty('Pops'))
+    p <- tagList(p, tags$hr(), tsPlots.makePopTable(data.currentSelection[['Pops']]))
+  p})
 
 # things to do when TSPlots panel is opened
 observeEvent(input$UIPanels, {
